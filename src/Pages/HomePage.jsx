92fccfb202a025da7c9bbd947963b562b476990e
Uday kiran Reddy <udaykiranbyreddy@gmail.com>
2024-11-01 08:00:56 +0530

    feat:Home page file added

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import ButtonAppBar from '../Components/Navbar'
+import Boards from '../Components/Boards'
+
+
+export default function HomePage() {
+  
+
+  return (
+    <>
+    <ButtonAppBar />
+    <Boards/>
+    </>
+  )
+}
