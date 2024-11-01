4c26e250f44ed8c58b194408044284eeb42af1a3
Uday kiran Reddy <udaykiranbyreddy@gmail.com>
2024-11-01 08:00:40 +0530

    feat:Error page file added

---
+++ src/Pages/ErrorPage.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import ButtonAppBar from '../Components/Navbar'
+import ErrorContent from '../Components/ErrorHandler/ErrorContent'
+
+export default function ErrorPage() {
+  return (
+    <>
+    <ButtonAppBar />
+    <ErrorContent />
+    </>
+  )
+}
