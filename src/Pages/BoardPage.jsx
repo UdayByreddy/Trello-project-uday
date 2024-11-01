4d940d4158ecacab1f27dca8b82ca3198769da81
Uday kiran Reddy <udaykiranbyreddy@gmail.com>
2024-11-01 08:00:21 +0530

    feat:Board page file added

---
+++ src/Pages/BoardPage.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import ButtonAppBar from '../Components/Navbar'
+import ListsItemsPage from './ListItemsPage'
+
+
+export default function BoardPage() {
+ 
+  return (
+    <>
+      <ButtonAppBar />
+      <ListsItemsPage />
+    </>
+  )
+}
