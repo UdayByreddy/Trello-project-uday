a3a19320fe03980d054b813bb65da8fe73f41716
Uday kiran Reddy <udaykiranbyreddy@gmail.com>
2024-11-02 18:08:46 +0530

    feat: added mainlayout in rotue

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@
 import React,{lazy, Suspense} from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+import MainLayout from './Layout/MainLayout';
+
 const HomePage = lazy(()=>import('./Pages/HomePage'))
 const BoardPage = lazy(()=>import('./Pages/BoardPage'))
 const ErrorPage = lazy(()=>import('./Pages/ErrorPage'))
@@ -14,9 +16,11 @@ export default function App() {
       <BrowserRouter>
       <Suspense fallback={<div>...Loading</div>}>
       <Routes>
+          <Route element={<MainLayout />} path='/'>
           <Route element={<HomePage  />} path='/' />
           <Route element={<BoardPage />} path='/border/:id' />
           <Route element={<ErrorPage />} path='*' />
+          </Route>
         </Routes>
 
       </Suspense>
